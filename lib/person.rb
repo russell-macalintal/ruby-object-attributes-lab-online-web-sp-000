6a92a6b685d82f08a5e75a6960a99f54cb8224a1
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-14 02:41:50 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name)
+    @per_name = name
+  end
+
+  def name
+    @per_name
+  end
+
+  def job=(job)
+    @per_job = job
+  end
+
+  def job
+    @per_job
+  end
+end
